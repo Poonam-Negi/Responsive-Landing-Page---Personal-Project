44a339c0864a6635ebc120b629b7948e361947d3
Poonam-Negi <ipoonam.negi21@gmail.com>
2026-03-09 13:17:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
+
+const themeToggle = document.getElementById("theme-toggle");
+const html = document.documentElement;
+
+if (localStorage.getItem("theme") === "dark") {
+  html.classList.add("dark");
+}
+
+themeToggle.addEventListener("click", () => {
+
+  html.classList.toggle("dark");
+
+  if (html.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+
+});
